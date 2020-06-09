bfb3a12b279a5585230af8abcf389be871456a31
Lorenalgm <lorena@proesc.com>
2020-06-08 22:55:31 -0300

    Adicionada estrutura do usuário e consulta ao github

---
+++ frontend/src/pages/User/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+export default function User() {
+    const username = localStorage.getItem('username');
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        axios.get(`https://api.github.com/users/${username}`).then(response => {
+            setUser(response.data);
+        });
+    }, [username]);
+
+    return (
+        <div className="container-user">
+            <div className="header">
+                <h1>Busca<span>Dev</span></h1>
+                <Link to="/home">
+                    <button>Início</button>
+                </Link>
+            </div>
+            {
+                user?
+                <div className="user">
+                <h1>Busca<span>Dev</span></h1>
+                <p>{user.login}</p>
+                <p>{user.bio}</p>
+                <p>{user.avatar_url}</p>
+                </div>
+
+                : 
+                
+                <div className="user-not-found">
+                    <h2>Usuário não encontrado :(</h2>
+                    <Link to="/home">
+                        <button>Tente novamente</button>
+                    </Link>
+                </div>
+            }
+            
+        </div>
+    )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
+import User from './pages/User';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/user" exact component={User} />
             </Switch>
         </BrowserRouter>
     )
