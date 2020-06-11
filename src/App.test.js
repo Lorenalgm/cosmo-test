3f92152c90869c0e7e171ba83b2242580ff0c76e
Lorenalgm <lorena@proesc.com>
2020-06-10 22:57:50 -0300

    Adicionado teste para consultar se o login foi renderizado

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Routes from './routes';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('render login page', () => {
+  const { getByText } = render(<Routes />);
+  const linkElement = getByText(/buscar/i);
   expect(linkElement).toBeInTheDocument();
 });
